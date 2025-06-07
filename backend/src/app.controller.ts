1f41908b4320b99273240a3bff0f0fb8d8bcb729
dheeraj7866 <93239707+dheeraj7866@users.noreply.github.com>
2025-06-07 23:03:59 +0530

    xv

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.schema';
 
 @Controller()
 export class AppController {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
   @Get()
   getHello(): string {
     return 'Hello from Backend!';
+  }
+
+  @Get('users')
+  async getUsers(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 }
